a0fafbeca4fd2e43d03efa4b1e675ec617c44b1c
YJiva <2280134514@qq.com>
2025-05-17 23:16:19 +0800

    nomsg

--- js/index.js
+++ js/index.js
@@ -5,22 +5,27 @@ var goflag=1
 drawerBtn.click(function(){
     if(goflag==1){
         drawerBtn.addClass("fa-rotate-180")
-        golr.css("cssText","right:10%")
+        golr.css("cssText","left:0%")
         goflag=0
     } else{
         drawerBtn.removeClass("fa-rotate-180")
-        golr.css("cssText","right:92%")
+        golr.css("cssText","left:-10%")
         goflag=1
     }
 })
+
+//default config
+defCFG=['面条', '饭', '馄dasdsad饨', '鸡排', '麻辣烫',
+  '粉条', '鸭血粉丝', '肯德基', '麦当劳', '不吃']
+
+
 //抽奖功能
 let getEle = document.getElementsByClassName.bind(document);
 let pointer = getEle('pointer')[0];
 let result = getEle('result')[0];
 let lights = Array.prototype.slice.call(getEle('light'));
 let onRotation = false; // 记录当前是否正在旋转，如果正在旋转，就不能继续点击了
-let reward = ['面条', '饭', '馄饨', '鸡排', '麻辣烫',
-'粉条', '鸭血粉丝', '肯德基', '麦当劳', '不吃'];
+let reward = defCFG;
 // 根据随机角度获取奖励
 let getReward = (function() {
   currentDeg = 0;
@@ -53,3 +58,87 @@ pointer.addEventListener('transitionend', () => {
   }, 500);
 // 这里每100毫秒为闪烁一次
 })
+//选项角度的计算
+
+// 获取目标元素
+const targetElement = document.querySelector('.sector');
+
+// 获取目标元素的父元素
+const parent = targetElement.parentElement;
+
+// 获取该父元素下所有子元素
+const siblings = parent.children;
+
+// 统计兄弟元素的数量
+let siblingCount = 0;
+
+for(let i = 0; i < siblings.length; i++) {
+    // 排除目标元素本身
+    if(siblings[i] !== targetElement) {
+        siblingCount++;
+    }
+}
+
+for(let i=0;i<siblingCount;i++){
+  
+}
+let newCFG=[]
+function foodAdd(foodinput){
+  
+  var count = $('#addhere').children().length;
+   if(count>=10){
+    alert("noooooo")
+}else{
+  newCFG.push(foodinput)
+  $("#addhere").append('<div class="addFoodBlock"><span>'+newCFG[newCFG.length-1]+'</span></div>');
+
+
+}
+
+
+}
+
+function update(CFG) {
+
+// nodefault
+ for(i=0;i<10;i++){
+ 
+
+  $(".panel").children().eq(i).children().children().text(CFG[i])
+ }
+ reward = CFG;
+
+  }
+
+ function foodRemove() {
+  newCFG=[]
+  reward = [];
+  $("#addhere").children().remove()
+  for(i=0;i<10;i++){
+     
+    
+    $(".panel").children().eq(i).children().children().text("")
+   }
+   } 
+
+   function resetFood(CFG) {
+
+    // nodefault
+     for(i=0;i<10;i++){
+     
+    
+      $(".panel").children().eq(i).children().children().text(CFG[i])
+     }
+     reward = CFG;
+    
+      }
+
+      let timer;
+      $('#FoodInput').on('keypress', function(e) {
+          if (e.which === 13) {
+              clearTimeout(timer); // 清除之前的延迟
+              timer = setTimeout(() => {
+                foodAdd($(this).val())
+              }, 100);
+          }
+      });
